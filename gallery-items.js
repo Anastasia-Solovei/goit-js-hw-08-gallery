75fe432a97a7c85a0b1d255ae5d56f472c0e4dbf
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-05-19 16:50:12 +0300

    task solution

--- gallery-items.js
+++ gallery-items.js
@@ -1,65 +1,83 @@
-export default [
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-    description: 'Hokkaido Flower',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-    description: 'Aerial Beach View',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    description: 'Flower Blooms',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-    description: 'Alpine Mountains',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    description: 'Mountain Lake Sailing',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    description: 'Alpine Spring Meadows',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    description: 'Nature Landscape',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    description: 'Lighthouse Coast Sea',
-  },
-];
+import items from './data/gallery-items.js';
+
+const galleryContainer = document.querySelector('ul.js-gallery');
+const lightbox = document.querySelector('div.lightbox');
+const lightboxOverlay = document.querySelector('div.lightbox__overlay');
+const lightboxImage = document.querySelector('img.lightbox__image');
+const lightboxCloseButton = document.querySelector(
+  'button[data-action="close-lightbox"]',
+);
+
+const galleryItemsMarkup = createGalleryItemsMarkup(items);
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+galleryContainer.addEventListener('click', onGalleryImageClick);
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }, index) => {
+      return `
+            <li class="gallery__item">
+                <a
+                    class="gallery__link"
+                    href="${original}"
+                >
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        data-index="${index}"
+                        alt="${description}"
+                    />
+                </a>
+            </li>`;
+    })
+    .join('');
+}
+
+function onGalleryImageClick(evt) {
+  evt.preventDefault();
+
+  onLightboxOpen(evt);
+}
+
+function onLightboxOpen(evt) {
+  const isGalleryImage = document.querySelector('img.gallery__image');
+  if (!isGalleryImage) {
+    return;
+  }
+
+  lightboxCloseButton.addEventListener('click', onLightboxClose);
+  lightboxOverlay.addEventListener('click', onLightboxClose);
+  window.addEventListener('keydown', onEscCodePress);
+  //   window.addEventListener('keydown', onArrowBtnPress);
+  lightbox.classList.add('is-open');
+  lightboxImage.src = evt.target.dataset.source;
+}
+
+function onLightboxClose() {
+  lightboxCloseButton.removeEventListener('click', onLightboxClose);
+  lightboxOverlay.removeEventListener('click', onLightboxClose);
+  window.removeEventListener('keydown', onEscCodePress);
+  //   window.removeEventListener('keydown', onArrowBtnPress);
+  lightbox.classList.remove('is-open');
+  lightboxImage.src = '';
+}
+
+function onEscCodePress(evt) {
+  if (evt.code === 'Escape') {
+    onLightboxClose();
+  }
+}
+
+// function onArrowBtnPress(evt) {
+//   const galleryImage = document.querySelector('img.gallery__image');
+
+//   if (evt.code === 'ArrowRight') {
+//     lightboxImage.index = galleryImage.dataset.index + 1;
+//   } else if (evt.code === 'ArrowLeft') {
+//     lightboxImage.index = galleryImage.dataset.index - 1;
+//   } else {
+//     return;
+//   }
+// }
